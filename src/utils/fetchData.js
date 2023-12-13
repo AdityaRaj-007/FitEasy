dc61665d7e90de5fa7436384c5015a10f4eb69a4
AdityaRaj-007 <iamadityaraj.2001@gmail.com>
2023-12-13 21:27:37 +0530

    feat: fitness app

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': '6d7f9ca43fmsh0f1882f7fde7bd9p144553jsn10f6338d05c2',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '6d7f9ca43fmsh0f1882f7fde7bd9p144553jsn10f6338d05c2',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
